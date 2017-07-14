dd3d90abba4a084dcc6ad49bf4257fc04954c33c
Eli Selkin <ejselkin@cpp.edu>
2017-07-14 11:27:09 -0700

    Initial Commit

---
+++ curbmap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  curbmap
+//
+//  Created by Eli Selkin on 7/14/17.
+//  Copyright © 2017 curbmap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
