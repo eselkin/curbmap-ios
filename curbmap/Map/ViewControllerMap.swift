962def2028bbd7eb1ef5a92fc7b680bbd2e68d63
Eli Selkin <ejselkin@cpp.edu>
2017-07-14 11:56:57 -0700

    Just the initial map view

---
+++ curbmap/Map/ViewControllerMap.swift
@@ -0,0 +1,28 @@
+//
+//  ViewControllerMap.swift
+//  curbmap
+//
+//  Created by Eli Selkin on 7/14/17.
+//  Copyright © 2017 curbmap. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewControllerMap: UIViewController {
+    let initialLocation = CLLocation(latitude: 34, longitude: -118)
+    let regionRadius: CLLocationDistance = 2000
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        centerMapOnLocation(location: initialLocation)
+    }
+    
+}
